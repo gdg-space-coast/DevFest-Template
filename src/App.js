20850dec163090589a450e0c15009a274e3d9296
Gil Creque <me@gilcreque.com>
2024-06-25 22:02:14 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import devfestOrlando from "./assets/2023/DevFest-2023-Logo.Central-Florida.png";
-import devfestMiami from "./assets/2023/DevFest-2023-Logo.Miami.png";
-import devfestTampa from "./assets/2023/DevFest-2023-Logo.Tampa.png";
-import CityBanner from './components/city-banner.js';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Home from "./pages/home";
 
 function App() {
   return (
     <Router>
-      <CityBanner color="bg-purple-900" url="https://orlando.devfestflorida.com" alt="DevFest Central Florida" src={devfestOrlando} date="October 14th, 2023" gdgWebsite="https://gdg.community.dev/gdg-central-florida/" twitter="GDGCentralFL" linkedIn="https://www.linkedin.com/company/google-developers-group-central-florida/" meetup="https://www.meetup.com/gdg-central-florida/" chapterName="Central Florida" />
-      <CityBanner color="bg-emerald-800" url="https://tampa.devfestflorida.com" alt="DevFest Tampa" src={devfestTampa} date="October 21st, 2023" gdgWebsite="https://gdg.community.dev/gdg-tampa-bay/" twitter="GDGSuncoast" linkedIn="https://www.linkedin.com/company/gdgsuncoast/" meetup="https://www.meetup.com/gdgtampabay/" chapterName="Tampa" />
-      
-      <CityBanner color="bg-pink-600" url="https://miami.devfestflorida.com" alt="DevFest Miami" src={devfestMiami} date="December 9th, 2023" gdgWebsite="https://gdg.community.dev/gdg-miami/" twitter="GDGMiami" linkedIn="https://www.linkedin.com/company/gdg-miami/" meetup="https://www.meetup.com/gdgmiami/" chapterName="Miami" />
+      <Home />
     </Router>
   );
 }
--- src/components/city-banner.js
+++ src/components/city-banner.js
@@ -1,29 +1,54 @@
-import React from 'react';
-import { FaGlobe, FaLinkedin, FaMeetup, FaTwitter } from 'react-icons/fa';
+import React from "react";
+import { FaGlobe, FaLinkedin, FaMeetup, FaTwitter } from "react-icons/fa";
 
+const CityBanner = ({
+  src,
+  alt,
+  date,
+  url,
+  color = "bg-black",
+  gdgWebsite,
+  twitter,
+  linkedIn,
+  meetup,
+  chapterName,
+  id,
+}) => (
+  <article
+    id={id}
+    className={`${color} text-black flex flex-row md:flex-col place-items-center justify-center p-1 md:p-5 lg:p-20 grow`}
+  >
+    <a href={url}>
+      <img src={src} className="p-6 md:p-2 lg:p-5" alt={alt} />
+      <h2 className="text-white text-center font-semibold w-100">{date}</h2>
+    </a>
 
-const CityBanner = ({ src, alt, date, url, color = "bg-black", gdgWebsite, twitter, linkedIn, meetup, chapterName }) => (
-    <article className={`${color} text-white flex flex-row md:flex-col place-items-center justify-center p-1 md:p-5 lg:p-20 h-1/3 sm:h-screen sm:w-1/3 grow`}>
-        <a href={url}>
-            <img src={src} className="p-6 md:p-2 lg:p-5" alt={alt} />
-            <h2 className='text-white text-center font-semibold w-100'>{date}</h2>
+    <ul className="hidden md:flex flex-row space-y-0 space-x-4 text-xl my-4 w-100 place-items-center justify-center">
+      <li className="m-0">
+        <a href={gdgWebsite} aria-label={`GDG ${chapterName} Website`}>
+          <FaGlobe />
         </a>
-
-        <ul className='hidden md:flex flex-row space-y-0 space-x-4 text-xl my-4 w-100 place-items-center justify-center'>
-            <li className='m-0'>
-                <a href={gdgWebsite} aria-label={`GDG ${chapterName} Website`}><FaGlobe /></a>
-            </li>
-            <li className='m-0'>
-                <a href={'https://twitter.com/' + twitter} aria-label={`Follow GDG ${chapterName} on Twitter`}><FaTwitter /></a>
-            </li>
-            <li className='m-0'>
-                <a href={linkedIn} aria-label={`Follow GDG ${chapterName} on LinkedIn`}><FaLinkedin /></a>
-            </li>
-            <li className='m-0'>
-                <a href={meetup} aria-label={`Follow GDG ${chapterName} on Meetup`}><FaMeetup /></a>
-            </li>
-        </ul>
-    </article>
+      </li>
+      <li className="m-0">
+        <a
+          href={"https://twitter.com/" + twitter}
+          aria-label={`Follow GDG ${chapterName} on Twitter`}
+        >
+          <FaTwitter />
+        </a>
+      </li>
+      <li className="m-0">
+        <a href={linkedIn} aria-label={`Follow GDG ${chapterName} on LinkedIn`}>
+          <FaLinkedin />
+        </a>
+      </li>
+      <li className="m-0">
+        <a href={meetup} aria-label={`Follow GDG ${chapterName} on Meetup`}>
+          <FaMeetup />
+        </a>
+      </li>
+    </ul>
+  </article>
 );
 
 export default CityBanner;
